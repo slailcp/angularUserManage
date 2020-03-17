4b84d78d3a866b0442d5f14db434385310ea5ebb
slailcp <1350033585@qq.com>
2020-03-17 11:14:13 +0800

    去掉无用代码

--- src/app/ngrx/reducers/index.ts
+++ src/app/ngrx/reducers/index.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { StoreModule, createSelector, createFeatureSelector, on } from '@ngrx/store';
 import * as loginInfoReducer from 'src/app/ngrx/reducers/login-info.reducer';
 import * as countReducer from 'src/app/ngrx/reducers/count.reducer';
@@ -14,20 +13,16 @@ export const initialState =  {
     countInfo: countReducer.initialState,
 }
 
-
 const reducers = { 
     loginInfo: loginInfoReducer.reducer,
     countInfo: countReducer.counterReducer,
 }
-
 
 // 获取登录信息中的用户姓名
 export const selectUsername = () => createSelector(
     createFeatureSelector<State, loginInfoReducer.State>('loginInfo'),
     (state: loginInfoReducer.State) => state.username
 );
-
-
 
 // 在State状态中筛选出countInfo
 // const selectCountInfo = (state: State) => state.countInfo;
@@ -38,22 +33,17 @@ export const selectFeatureCount = createSelector(
   (state: countReducer.State,userName: string) => `${userName}:${state.count}`, // state为上一级流下来的countInfo,userName为上一级流下来的state.loginInfo.username
 );
 
-
 export const getCount = () => 
 createSelector(
    (state, props) => state.countInfo[props.id],
    (counter, props) => counter * props.multiply // 此处的counter为上一级流下来的count
 )
 
-
 export const selectFeature = createFeatureSelector<State, countReducer.State>('countInfo');
 export const selectFeatureCounts = ()=> createSelector(
   selectFeature,
   (state: countReducer.State) => state.count
 );
-
-
-
 
 @NgModule({
     imports: [
